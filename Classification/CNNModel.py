d4459cfd1d4f877e9b1bfda622b049eea535c0a3
Neelkant Newra <newra008@gmail.com>
2022-04-15 10:43:00 +0530

    Update CNNModel.py

--- Classification/CNNModel.py
+++ Classification/CNNModel.py
@@ -73,6 +73,77 @@ def AlexNet(input_shape):
     model.add(Dense(4,input_dim=input_shape[0],activation='softmax'))
     model.compile(loss = 'categorical_crossentropy',optimizer = Adam(learning_rate=1e-3) , metrics = ['accuracy',Precision(),Recall()])
     return model
+
+
+# MobileNet 
+
+def convBlock(tensor, channels, strides, alpha=1.0, name=''):
+    channels = int(channels * alpha)
+
+    x = Conv2D(channels,
+               kernel_size=(3, 3),
+               strides=strides,
+               use_bias=False,
+               padding='same',
+               name='{}_conv'.format(name))(tensor)
+    x = BatchNormalization(name='{}_bn'.format(name))(x)
+    x = Activation('relu', name='{}_act'.format(name))(x)
+    return x
+
+
+def dwSepBlock(tensor, channels, strides, alpha=1.0, name=''):
+    """Depthwise separable conv: Depthwise conv followed by Pointwise conv."""
+    channels = int(channels * alpha)
+
+    # Depthwise
+    x = DepthwiseConv2D(kernel_size=(3, 3),
+                        strides=strides,
+                        use_bias=False,
+                        padding='same',
+                        name='{}_dw'.format(name))(tensor)
+    x = BatchNormalization(name='{}_bn1'.format(name))(x)
+    x = Activation('relu', name='{}_act1'.format(name))(x)
+
+    # Pointwise
+    x = Conv2D(channels,
+               kernel_size=(1, 1),
+               strides=(1, 1),
+               use_bias=False,
+               padding='same',
+               name='{}_pw'.format(name))(x)
+    x = BatchNormalization(name='{}_bn2'.format(name))(x)
+    x = Activation('relu', name='{}_act2'.format(name))(x)
+    return x
+
+def MobileNet(input_shape, num_classes, alpha=1.0 ):
+    x_in = Input(shape=input_shape)
+
+    x = convBlock(x_in, 32, (2, 2), alpha=alpha, name='initial')
+
+    layers = [
+        (64, (1, 1)),
+        (128, (2, 2)),
+        (128, (1, 1)),
+        (256, (2, 2)),
+        (256, (1, 1)),
+        (512, (2, 2)),
+        *[(512, (1, 1)) for _ in range(5)],
+        (1024, (2, 2)),
+        (1024, (2, 2))
+    ]
+
+    for i, (channels, strides) in enumerate(layers):
+        x = dwSepBlock(x, channels, strides, alpha=alpha, name='block{}'.format(i))
+        
+    x = GlobalAveragePooling2D(name='global_avg')(x)
+    x = Dense(num_classes, activation='softmax', name='softmax')(x)
+
+    model = Model(inputs=x_in, outputs=x)
+        
+    model.compile(loss = 'categorical_crossentropy',optimizer = Adam(learning_rate=1e-3) , metrics = ['accuracy',Precision(),Recall()])
+    return model
+
+
   
 # Base CNN model
 def CNN(input_shape):
